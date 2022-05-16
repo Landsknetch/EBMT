fc59894d786f1df04cd8d70a52619f1eb5bc5ae0
Taufiq Gilang <adhitama1310@gmail.com>
2022-05-16 21:27:49 +0700

    test dropdown 1

--- pages/remitansi/index.jsx
+++ pages/remitansi/index.jsx
@@ -307,7 +307,7 @@ export default function Remitansi()
                     </div>
                   </div>
 
-                  <div className="form-group row">
+                  <div className="form-group">
                     <label htmlFor="inputPassword3" className="col-sm-2 col-form-label">Rekening Bank Penerima</label>
                     <div className="col-4">
                       <select className="custom-select">
